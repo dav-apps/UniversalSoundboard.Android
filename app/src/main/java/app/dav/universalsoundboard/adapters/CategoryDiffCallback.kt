a6cbe68bfd7e1ddc807c41ffef56614fe90cc688
David <dav2070@web.de>
2018-09-07 19:12:11 +0200

    Write code for category list adapter and category diff callback

--- app/src/main/java/app/dav/universalsoundboard/adapters/CategoryDiffCallback.kt
+++ app/src/main/java/app/dav/universalsoundboard/adapters/CategoryDiffCallback.kt
@@ -1,4 +1,17 @@
 package app.dav.universalsoundboard.adapters
 
-class CategoryDiffCallback {
+import android.support.v7.util.DiffUtil
+import app.dav.universalsoundboard.models.Category
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category?, newItem: Category?): Boolean {
+        if(oldItem != null && newItem != null){
+            return oldItem.uuid.equals(newItem.uuid)
+        }
+        return false
+    }
+
+    override fun areContentsTheSame(oldItem: Category?, newItem: Category?): Boolean {
+        return oldItem == newItem
+    }
 }
--- app/src/main/java/app/dav/universalsoundboard/adapters/CategoryListAdapter.kt
+++ app/src/main/java/app/dav/universalsoundboard/adapters/CategoryListAdapter.kt
@@ -1,4 +1,37 @@
 package app.dav.universalsoundboard.adapters
 
-class CategoryListAdapter {
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.dav.universalsoundboard.databinding.CategoryListItemBinding
+import app.dav.universalsoundboard.models.Category
+import app.dav.universalsoundboard.models.Sound
+
+class CategoryListAdapter(
+        private val onItemClickListener: OnItemClickListener)
+    : ListAdapter<Category, CategoryListAdapter.ViewHolder>(CategoryDiffCallback()){
+
+    interface OnItemClickListener {
+        fun onItemClicked(category: Category)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(CategoryListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(View.OnClickListener {
+            onItemClickListener.onItemClicked(item)
+        }, item)
+    }
+
+    inner class ViewHolder(private val binding: CategoryListItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(onClickListener: View.OnClickListener, item: Category){
+            binding.onClickListener = onClickListener
+            binding.category = item
+        }
+    }
 }
