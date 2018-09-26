141db51f379411e699563a4dd2cccee516b67382
David <dav2070@web.de>
2018-09-26 17:45:37 +0200

    Add version and copyright texts and links to the privacy policy and GitHub to the Settings

--- app/src/main/java/app/dav/universalsoundboard/fragments/SettingsFragment.kt
+++ app/src/main/java/app/dav/universalsoundboard/fragments/SettingsFragment.kt
@@ -3,11 +3,13 @@ package app.dav.universalsoundboard.fragments
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.dav.universalsoundboard.R
 import app.dav.universalsoundboard.viewmodels.SettingsViewModel
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 class SettingsFragment : Fragment() {
     private lateinit var viewModel: SettingsViewModel
@@ -15,7 +17,9 @@ class SettingsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        privacy_policy_textview.movementMethod = LinkMovementMethod.getInstance()
+        usb_on_github_textview.movementMethod = LinkMovementMethod.getInstance()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
