6121361594f5787a6c6ad5e1ae6eecea9c7cb90c
David <dav2070@web.de>
2018-09-26 14:26:12 +0200

    Add SettingsFragment

---
+++ app/src/main/java/app/dav/universalsoundboard/fragments/SettingsFragment.kt
@@ -0,0 +1,31 @@
+package app.dav.universalsoundboard.fragments
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import app.dav.universalsoundboard.R
+
+class SettingsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SettingsFragment()
+    }
+
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.settings_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/app/dav/universalsoundboard/viewmodels/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package app.dav.universalsoundboard.fragments
+
+import android.arch.lifecycle.ViewModel;
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
