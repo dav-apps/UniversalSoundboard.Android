0549eeda556f1a39cd56100aa200e713eb5a9d46
David <dav2070@web.de>
2018-11-24 20:10:57 +0100

    Add login button
    Add functionality for the login button which redirects the user to the browser and opens the login page

--- app/src/main/java/app/dav/universalsoundboard/fragments/AccountFragment.kt
+++ app/src/main/java/app/dav/universalsoundboard/fragments/AccountFragment.kt
@@ -1,16 +1,30 @@
 package app.dav.universalsoundboard.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.dav.universalsoundboard.R
+import app.dav.universalsoundboard.data.FileManager
+import kotlinx.android.synthetic.main.fragment_account.*
 
 class AccountFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        login_button.setOnClickListener {
+            val redirectUrl = "universalsoundboard:///"
+            val url: Uri = Uri.parse(FileManager.loginImplicitUrl + "?api_key=" + FileManager.apiKey + "&redirect_url=" + redirectUrl)
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            val packageManager = activity?.packageManager ?: return@setOnClickListener
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
