26fa0c669720299bc15e014eb0c048fd75cc429f
David <dav2070@web.de>
2019-02-26 23:20:58 +0100

    Update the SetCategory Dialog to support multiple categories per sound and update texts

--- app/src/main/java/app/dav/universalsoundboard/fragments/SetCategoryDialogFragment.kt
+++ app/src/main/java/app/dav/universalsoundboard/fragments/SetCategoryDialogFragment.kt
@@ -2,9 +2,9 @@ package app.dav.universalsoundboard.fragments
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.widget.Button
 import app.dav.universalsoundboard.R
 import app.dav.universalsoundboard.data.FileManager
 import app.dav.universalsoundboard.models.Category
@@ -12,61 +12,60 @@ import app.dav.universalsoundboard.models.Sound
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.util.*
 
 
 class SetCategoryDialogFragment : DialogFragment() {
     var sound: Sound? = null
-    var selectedItem = -1
-    var firstSelectedItem = -1
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return createDialog() ?: super.onCreateDialog(savedInstanceState)
     }
 
     private fun createDialog() : AlertDialog?{
-        if(sound == null) return null
-        val categories = FileManager.itemViewHolder.categories.value ?: return null
-        val categoriesArrayList = ArrayList<Category>()
+        val s = sound ?: return null
+        val allCategories = FileManager.itemViewHolder.categories.value ?: return null
+        val categories = ArrayList<Category>()
+        val selectedCategoriesBooleanArray = BooleanArray(allCategories.size - 1)
 
+        // Create the categories lists
         var i = -1
-        for(c in categories){
-            categoriesArrayList.add(c)
+        for(category in allCategories){
+            if(++i == 0) continue
+            categories.add(category)
 
-            if(c.uuid == sound?.category?.uuid) {
-                firstSelectedItem = i
-                selectedItem = i
+            if(s.categories.any { c -> c.uuid == category.uuid}){
+                // The sound belongs to the current category; add the category to the selected categories
+                selectedCategoriesBooleanArray.set(i - 1, true)
             }
-            i++
         }
 
-        categoriesArrayList.removeAt(0)
-        val categoriesNamesArray = Array<CharSequence>(categoriesArrayList.size) {
-            categoriesArrayList[it].name
+        val categoriesNamesArray = Array<CharSequence>(categories.size) {
+            categories[it].name
         }
-        var positiveButton: Button? = null
 
         val alertDialog = AlertDialog.Builder(activity)
-                .setTitle(getString(R.string.set_category_dialog_title))
-                .setSingleChoiceItems(categoriesNamesArray, selectedItem) { dialog, which ->
-                    selectedItem = which
-
-                    positiveButton?.isEnabled = selectedItem != firstSelectedItem
-                }
+                .setTitle(getString(R.string.set_category_dialog_title, s.name))
+                .setMultiChoiceItems(categoriesNamesArray, selectedCategoriesBooleanArray) { dialogInterface: DialogInterface, index: Int, isChecked: Boolean -> }
                 .setPositiveButton(R.string.set_category_dialog_positive_button_text) { dialog, which ->
-                    if(selectedItem == -1) return@setPositiveButton
-                    val s = sound ?: return@setPositiveButton
-                    val selectedCategory = categoriesArrayList.get(selectedItem)
+                    // Get the selected categories
+                    val selectedCategories = ArrayList<UUID>()
+                    var i = 0
+                    selectedCategoriesBooleanArray.forEach {
+                        if(it){
+                            selectedCategories.add(categories[i].uuid)
+                        }
+                        i++
+                    }
 
+                    // Save the new categories of the sound
                     GlobalScope.launch(Dispatchers.Main) {
-                        FileManager.setCategoryOfSound(s.uuid, selectedCategory.uuid)
+                        FileManager.setCategoriesOfSound(s.uuid, selectedCategories)
                         FileManager.itemViewHolder.loadSounds()
                     }
                 }
                 .setNegativeButton(R.string.dialog_negative_button, null)
                 .show()
-
-        positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
-        positiveButton.isEnabled = false
 
         return alertDialog
     }
