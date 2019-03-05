bff5eb130340d95c302672e9fe7c09d5844289fd
David <dav2070@web.de>
2019-03-05 18:32:02 +0100

    Fix tests with a AndroidX

--- app/src/androidTest/java/app/dav/universalsoundboard/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/dav/universalsoundboard/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package app.dav.universalsoundboard
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,7 +16,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("app.dav.universalsoundboard", appContext.packageName)
     }
 }
--- app/src/test/java/app/dav/universalsoundboard/ExampleUnitTest.kt
+++ app/src/test/java/app/dav/universalsoundboard/ExampleUnitTest.kt
@@ -1,7 +1,6 @@
 package app.dav.universalsoundboard
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
 /**
